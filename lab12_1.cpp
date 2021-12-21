d4337932f39dced9cae4d9cc6d63a2dc7bcd05d4
thidayu <tanthidayu_0000@hotmail.com>
2021-12-21 16:28:24 +0700

    "lab12.1 finished"

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    int a;
+    string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int b = 0; b < 3; b++)
+        cin.get();
+    srand(time(0));
+    a = rand()%9;
+    cout << "You will get " << grade[a] << " in this 261102.";
+    return 0;
+}
